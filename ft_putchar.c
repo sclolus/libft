157f7c1f0d5d0aa55750ff6636feb395215305e4
Sebastien CLOLUS <sclolus@e3r11p9.42.fr>
2016-11-28 03:06:14 +0100

    Mod de ft_putchar (PUTAIN LOL)

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,23 +6,14 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 11:41:00 by sclolus           #+#    #+#             */
-/*   Updated: 2016/11/07 11:49:21 by sclolus          ###   ########.fr       */
+/*   Updated: 2016/11/28 03:04:36 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-void	ft_putchar(char c)
+void	ft_putchar(unsigned char c)
 {
-	unsigned char	utf[2];
-
-	if (!ft_isascii(c))
-	{
-		utf[0] = 0xC0 | ((unsigned char)c >> 6);
-		utf[1] = 0x80 | (0x3F & (unsigned char)c);
-		write(1, utf, 2);
-	}
-	else
-		write(1, &c, 1);
+	write(1, &c, 1);
 }
