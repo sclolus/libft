8df3d385124ba5d81cea73f5f7239832fd377b60
Sebastien CLOLUS <seb.miniske@gmail.com>
2016-12-07 20:25:54 +0100

    Ajout de ft_strlen <gnulib.c>

--- ft_strlen.c
+++ ft_strlen.c
@@ -5,19 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/04 11:41:45 by sclolus           #+#    #+#             */
-/*   Updated: 2016/11/07 19:04:52 by sclolus          ###   ########.fr       */
+/*   Created: 2016/11/13 15:33:53 by sclolus           #+#    #+#             */
+/*   Updated: 2016/12/01 05:43:44 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+static size_t	ft_sus_addr(unsigned long *longword_ptr, const char *s)
 {
-	size_t	i;
+	char *tmp;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	tmp = (char *)(longword_ptr - 1);
+	if (!tmp[0])
+		return (tmp - s);
+	else if (!tmp[1])
+		return (tmp - s + 1);
+	else if (!tmp[2])
+		return (tmp - s + 2);
+	else if (!tmp[3])
+		return (tmp - s + 3);
+	else if (!tmp[4])
+		return (tmp - s + 4); 
+	else if (!tmp[5])
+		return (tmp - s + 5);
+	else if (!tmp[6])
+		return (tmp - s + 6);
+	else
+		return (tmp - s + 7);
+}
+
+size_t			ft_strlen(const char *s)
+{
+	unsigned long	*longword_ptr;
+	unsigned char	*tmp;
+	unsigned long	longword;
+	unsigned long	magicbit;
+	unsigned long	tester;
+
+	magicbit = (0x7efefefeL << 32) | 0xfefefeffL;
+	tester = ~magicbit;
+	tmp = (unsigned char*)s;
+	while ((unsigned long)tmp & (sizeof(unsigned long) - 1))
+	{		
+		++tmp;
+		if (!*tmp)
+			return ((unsigned char *)tmp - (unsigned char*)s);
+	}
+	longword_ptr = (unsigned long*)tmp;
+	while (1)
+	{
+		longword = *longword_ptr++;
+		if (((longword + magicbit) ^ ~longword) & tester)
+			return (ft_sus_addr(longword_ptr, s));
+	}
 }
