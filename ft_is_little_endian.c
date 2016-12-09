2fa62a7adf9b2904f4ee4e61e1faf576ff2b3468
Sebastien CLOLUS <sclolus@e3r12p14.42.fr>
2016-12-09 04:24:58 +0100

    Ajout de ft_log2.c et de ft_is_little_endian.c

--- ft_is_little_endian.c
+++ ft_is_little_endian.c
@@ -1,33 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_page.c                                    :+:      :+:    :+:   */
+/*   ft_is_little_endian.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/06 17:39:15 by sclolus           #+#    #+#             */
-/*   Updated: 2016/12/09 04:23:22 by sclolus          ###   ########.fr       */
+/*   Created: 2016/12/09 04:20:02 by sclolus           #+#    #+#             */
+/*   Updated: 2016/12/09 04:24:03 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_print_page(void *addr)
+unsigned int	ft_is_little_endian(void)
 {
-	unsigned char	*tmp;
-	int				i;
+	short	tester;
 
-	i = 0;
-	tmp = addr - ((unsigned long)4096 - 1 & (unsigned long)addr);
-	while (i < 4096)
-	{
-		if (ft_isprint(tmp[i]))
-			ft_putchar(tmp[i]);
-		else
-			ft_putchar('.');
-		i++;
-		if (!(i % 64))
-			ft_putchar('\n');
-	}
-	ft_putchar('\n');
+	tester = 0x0001;
+	return (*(char*)&tester == 0x01);
 }
--- ft_log2.c
+++ ft_log2.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_page.c                                    :+:      :+:    :+:   */
+/*   ft_log2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/06 17:39:15 by sclolus           #+#    #+#             */
-/*   Updated: 2016/12/09 04:23:22 by sclolus          ###   ########.fr       */
+/*   Created: 2016/12/09 03:58:36 by sclolus           #+#    #+#             */
+/*   Updated: 2016/12/09 04:19:40 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_print_page(void *addr)
+unsigned int	ft_log2(unsigned long long nbr)
 {
-	unsigned char	*tmp;
-	int				i;
+	int	i;
 
 	i = 0;
-	tmp = addr - ((unsigned long)4096 - 1 & (unsigned long)addr);
-	while (i < 4096)
-	{
-		if (ft_isprint(tmp[i]))
-			ft_putchar(tmp[i]);
-		else
-			ft_putchar('.');
+	while (nbr >>= 1)
 		i++;
-		if (!(i % 64))
-			ft_putchar('\n');
-	}
-	ft_putchar('\n');
+	return(i);
 }
