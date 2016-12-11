9431f02b4f4f6733f7d5ab2377ee13bc698c97e7
Sebastien CLOLUS <sclolus@e3r11p18.42.fr>
2016-12-11 18:07:36 +0100

    mod de libft.h

--- libft.h
+++ libft.h
@@ -6,20 +6,14 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 11:35:25 by sclolus           #+#    #+#             */
-/*   Updated: 2016/12/11 18:03:27 by sclolus          ###   ########.fr       */
+/*   Updated: 2016/12/11 17:35:50 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
-
-typedef struct	s_list
-{
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}				t_list;
+# include "get_next_line.h"
 
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
