783e609d2af17b913c640f8276943c7ab3353cc4
Sebastien CLOLUS <sclolus@e3r13p11.42.fr>
2016-12-14 02:21:49 +0100

    Ajout de ft_lstlen.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 11:35:25 by sclolus           #+#    #+#             */
-/*   Updated: 2016/12/11 22:48:06 by sclolus          ###   ########.fr       */
+/*   Updated: 2016/12/14 02:20:30 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,7 @@ void			ft_putendl_fd(char const *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 
 t_list			*ft_lstnew(void const *content, size_t content_size);
+unsigned int	ft_lstlen(t_list *lst);
 void			ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
