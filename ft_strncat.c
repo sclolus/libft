ece802b769e5b373fec4d0fb8ab0ab7825de1676
Sebastien CLOLUS <sclolus@e3r11p12.42.fr>
2016-12-21 02:26:27 +0100

    Optimisation de ft_strncat.c

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 13:34:34 by sclolus           #+#    #+#             */
-/*   Updated: 2016/12/21 02:07:49 by sclolus          ###   ########.fr       */
+/*   Updated: 2016/12/21 02:24:54 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	i = ft_strlen(s2);
+	n = n > i ? i : n;
 	i = ft_strlen(s1);
 	ft_strncpy(s1 + i, s2, n);
 	s1[i + n] = '\0';
