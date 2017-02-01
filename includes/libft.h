6286272098a8086b611d2abcf34a5c8dc6a71472
Sebastien CLOLUS <seb.miniske@gmail.com>
2017-02-01 02:09:24 +0100

    Ajout de ft_strjoin_f.c

--- includes/libft.h
+++ includes/libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <string.h>
 # include <stdint.h>
+# include <stdlib.h>
 # define PHASZERO(x) ((((x + (0x7f7f7f7f7f7f7f7f)) ^ ~x) & 0x818080808080808080)
 # define PHASN(x, n) (Phaszero(x ^ ((~0UL / 255L) * n)))
 
@@ -70,6 +71,7 @@ int				ft_strequ(char const *s1, char const *s2);
 int				ft_strnequ(char const *s1, char const *s2, size_t n);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strjoin_f(char *a, char *b, int32_t mode);
 char			*ft_strtrim(char const *s);
 char			**ft_strsplit(char const *s, char c);
 void			ft_putchar(unsigned char c);
--- libft.h
+++ libft.h
@@ -71,6 +71,7 @@ int				ft_strequ(char const *s1, char const *s2);
 int				ft_strnequ(char const *s1, char const *s2, size_t n);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strjoin_f(char *a, char *b, int32_t mode);
 char			*ft_strtrim(char const *s);
 char			**ft_strsplit(char const *s, char c);
 void			ft_putchar(unsigned char c);
