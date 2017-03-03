d71a9599bcaa25148b757895fce19250890b6639
Sebastien CLOLUS <sclolus@e2r11p21.42.fr>
2017-03-03 04:54:22 +0100

    dump

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 17:08:18 by sclolus           #+#    #+#             */
-/*   Updated: 2017/03/03 04:21:00 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/03/03 04:53:58 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_lstadd(t_list **alst, t_list *new)
 {
 	new->next = *alst;
 	*alst = new;
-	new->next->prev = new;
+	if (new->next)
+		new->next->prev = new;
 	(*alst)->prev = NULL;
 }
